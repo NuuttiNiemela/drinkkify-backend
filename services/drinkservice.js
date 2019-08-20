244a3fec3ce6ecc745009e1bd1a0b27c5a511d81
Heikki Vihemäki <heikki.vihemaki@gmail.com>
2019-08-20 17:22:00 +0300

    osoite drinks -> get post ja get idllä. indeksissä.

--- services/drinkservice.js
+++ services/drinkservice.js
@@ -27,4 +27,24 @@ function getAllDrinks() {
         );
 }
 
-module.exports = {getAllDrinks};
+function addDrink(newdrink) {
+    const insertStmt = "INSERT INTO drinks(drink_name, drink_instructions) VALUES($1, $2)  RETURNING id";
+    return pool.connect()
+        .then(client => {
+                return client.query(insertStmt, [newdrink.drink_name, newdrink.drink_instructions])
+                    .then((vastaus) => {
+                            client.release();
+                            console.log("Insertoitu vastaus", vastaus.rows);
+                            return vastaus.rows[0].id;
+                        }
+                    )
+                    .catch(e => {
+                        console.log("dbuserservice:post virhe", e.message);
+                        throw new Error(e.message)
+                    })
+            }
+        );
+}
+
+
+module.exports = {getAllDrinks, addDrink};
