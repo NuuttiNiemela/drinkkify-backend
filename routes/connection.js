ac816fae3902f720d5212c8ef6e42dc35d9b6b22
Heikki Vihemäki <heikki.vihemaki@gmail.com>
2019-08-21 13:43:34 +0300

    delete, post lisätty..

--- routes/connection.js
+++
@@ -1,9 +0,0 @@
-require ('dotenv').config();
-const pg = require('pg');
-const {PGUSER, PGHOST, PGPORT} = process.env;
-
-if (process.env.NODE_ENV === 'development')
-    console.log(`Connecting to ${PGUSER} @ ${PGHOST}:${PGHOST}`);
-
-module.exports = new pg.Pool();
-
